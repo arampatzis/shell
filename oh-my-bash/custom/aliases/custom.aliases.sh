4c8f77d8b8f7f185dea17a3efea135a7786b9f1d
G. Arampatzis <gtarabat@gmail.com>
2021-03-04 19:22:44 +0100

    add epicirus related

--- oh-my-bash/custom/aliases/custom.aliases.sh
+++ oh-my-bash/custom/aliases/custom.aliases.sh
@@ -1,5 +1,3 @@
-# Add your own custom alias in the custom/aliases directory. Aliases placed
-# here will override ones with the same name in the main alias directory.
 
 alias ls='ls  --group-directories-first --color --human-readable'
 alias ll='ls -l'             # Preferred 'ls' implementation
@@ -9,25 +7,8 @@ alias grep='grep -n --color=always'
 alias bashrc='vim ~/.bashrc;  source ~/.bashrc'
 alias vimrc='vim ~/.vimrc'
 
-alias cdd='cd $HOME/Desktop/'
-alias cdp='cd $HOME/work/ETH-work/projects'
-alias cdg='cd $HOME/work/ETH-work/projects/2020-graph-sir/python'
-alias cdc='cd $HOME/work/projects/covid19'
-alias cdpiole='cd /Users/garampat/Documents/MATLAB/Sampling/piolet'
-
-alias updatedb='sudo /usr/libexec/locate.updatedb'
-
-alias updateallpython='python3 -m pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1 | cut -d : -f 2 | xargs -n1 pip install -U'
-
-alias matlab='/Applications/MATLAB_R2019a.app/bin/matlab &'
-
-export KORALI=$HOME/work/ETH-work/projects/korali/
-alias cdk='cd $KORALI'
-alias cda='cd $KORALI_APPS'
-
 alias daint='ssh -L 5555:daint.cscs.ch:22 garampat@ela.cscs.ch'
 alias ela='ssh -Y ela.cscs.ch'
-
 alias falcon='ssh -Y falcon.ethz.ch'
 alias euler='ssh -Y euler.ethz.ch'
 alias panda='ssh -Y panda.ethz.ch'
@@ -35,12 +16,22 @@ alias barry='ssh -Y barry.ethz.ch'
 
 alias pythonpath='echo -e ${PYTHONPATH//:/\\n}'
 
-HOST=`hostname -d`
-if [[ $HOST == "euler.ethz.ch" ]]; then
-  echo "Setting aliases on $HOST"
 
+if [[ $HOST_NAME == "epicurus" ]]; then
+  alias cdd='cd $HOME/Desktop/'
+  alias cdp='cd $HOME/work/ETH-work/projects'
+  alias cdg='cd $HOME/work/ETH-work/projects/2020-graph-sir/python'
+  alias cdcode='cd $HOME/work/ETH-work/codes'
+  alias cdpiole='cd /Users/garampat/Documents/MATLAB/Sampling/piolet'
+  alias updatedb='sudo /usr/libexec/locate.updatedb'
+  KORALI=$HOME/work/ETH-work/projects/korali/
+  alias cdk='cd $KORALI'
+  alias cda='cd $KORALI_APPS'
+fi
+
+
+if [[ $HOST_NAME == "euler.ethz.ch" ]]; then
   alias cds='cd /cluster/scratch/garampat'
-
   alias bjob='bjobs' #'bjobs -w'
   alias running_users='busers `bugroup | grep "^es_koumo"`'
   alias allocate='bsub -n 4  -Is bash'
--- oh-my-bash/custom/exports.sh
+++ oh-my-bash/custom/exports.sh
@@ -4,41 +4,33 @@
 # @Last modified by:   garampat
 # @Last modified time: 2021-02-28T22:07:08+01:00
 
-
-# export BASH_SILENCE_DEPRECATION_WARNING=1
-
 export HISTFILESIZE=1000000
 export HISTSIZE=1000000
-
-export PIOLE=$HOME/Documents/MATLAB/Sampling/piolet
-
-export CXX=g++-10
-export CC=gcc-10
-
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-
-export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/Cellar/gsl/2.6/lib/"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/Cellar/gsl/2.6/lib/"
-export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/usr/local/Cellar/gsl/2.6/lib/"
-
-#----------------------------------------------------------------------------------
-# GNU flavored commands. e.g. ls
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
-export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}"
-#----------------------------------------------------------------------------------
-
-# Setting PATH for Python 3.9
-export PATH="/Library/Frameworks/Python.framework/Versions/3.9/bin:${PATH}"
-
-export LIBRARY_PATH="$LIBRARY_PATH:`brew --prefix llvm`/lib"
 
 export FZF_DEFAULT_COMMAND='find .'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 
-HOST=`hostname -d`
-if [[ $HOST == "euler.ethz.ch" ]]; then
+if [[ $HOST_NAME == "epicurus" ]]; then
+  export CXX=g++-10
+  export CC=gcc-10
 
+  export PATH="/Library/Frameworks/Python.framework/Versions/3.9/bin:${PATH}"
+  export PATH="/usr/local/opt/llvm/bin:$PATH"
+
+  export LIBRARY_PATH="$LIBRARY_PATH:`brew --prefix llvm`/lib"
+  
+  # GNU flavored commands. e.g. ls
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
+  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:${MANPATH}"
+
+  export LIBRARY_PATH="$LIBRARY_PATH:/usr/local/Cellar/gsl/2.6/lib/"
+  export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/local/Cellar/gsl/2.6/lib/"
+  export DYLD_LIBRARY_PATH="$DYLD_LIBRARY_PATH:/usr/local/Cellar/gsl/2.6/lib/"
+fi
+
+
+if [[ $HOST_NAME == "euler.ethz.ch" ]]; then
   export PATH=$PATH:$HOME/bin
   export PATH=$HOME/usr/bin:$PATH
   export PATH=$HOME/usr/ctags/bin:$PATH
@@ -47,6 +39,5 @@ if [[ $HOST == "euler.ethz.ch" ]]; then
 
   export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usr/lib
   export XDG_CACHE_HOME=$HOME/.cache
-
 fi
 
