c7ba9cf39645f168aaa262c8768d5063209b407d
G. Arampatzis <gtarabat@gmail.com>
2022-01-20 18:35:43 +0100

    Add python venv plugin.

--- oh-my-bash/custom/plugins/mountit/mountit.plugin.sh
+++ oh-my-bash/custom/plugins/mountit/mountit.plugin.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+# File              : mountit.plugin.sh
+# Author            : George Arampatzis <garampat@ethz.ch>
+# Date              : 18.01.2022 20:23
+# Last Modified Date: 18.01.2022 20:23
+# Last Modified By  : George Arampatzis <garampat@ethz.ch>
+
 mountit() {
   mkdir -p .$1
   sshfs -o allow_other,defer_permissions,IdentityFile=~/.ssh/id_rsa  garampat@$1.ethz.ch: .$1
