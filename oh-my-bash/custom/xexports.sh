f6aab91b4521b1bbed13c9166f29837a2cd290ba
G. Arampatzis <gtarabat@gmail.com>
2025-04-01 14:44:18 +0300

    Changes from astakos

--- oh-my-bash/custom/xexports.sh
+++ oh-my-bash/custom/xexports.sh
@@ -52,8 +52,6 @@ if [[ $HOSTNAME == *"astakos"* ]]; then
 
     path_prepend "$HOME/local/bin"
 
-    source .cargo/env
-
     export PYTHON_KEYRING_BACKEND="keyring.backends.null.Keyring"
 
     . "$HOME/.cargo/env"
